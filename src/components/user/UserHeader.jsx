755de2839e26df7d4162aa577b09a1396cc140b2
Sumayya <sumayyaom18@gmail.com>
2024-09-10 13:33:38 +0530

    Fourth commit

--- src/components/user/UserHeader.jsx
+++ src/components/user/UserHeader.jsx
@@ -1,30 +1,4 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import Mode from '../Mode';
-// import { CircleUserRound } from 'lucide-react';
 
-// export default function UserHeader() {
-//   return (
-//     <div className='flex items-center justify-between w-full h-32 px-20 shadow-xl'>
-//         <div>
-//             <h1 className='text-4xl font-bold'>Car Rental</h1>
-//         </div>
-
-//     <nav className='flex gap-10 font-semibold'>
-//         <Link to="/user/home">Home</Link>
-//         <Link to="/user/about">About</Link>
-//         <Link to="/user/search">Search</Link>
-//         <Link to="/user/cars">Cars</Link>
-//         <Link to="/user/carDetails/:id">CarDetails</Link>
-//     </nav>
-
-//     <div className='flex items-center gap-8'>
-//         <Mode />
-//         <Link to="/user/profile"><CircleUserRound /></Link>
-//     </div>
-//     </div>
-//   )
-// }
 
 import React from 'react';
 import { Link } from 'react-router-dom';
