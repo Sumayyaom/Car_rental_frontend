e8c400b7fa1df2acdc448de9f1ab1db12eae38df
Sumayya <sumayyaom18@gmail.com>
2025-05-16 20:54:24 +0530

    Commit after changing HomeBanner images

--- src/pages/user/HomeBanner.jsx
+++ src/pages/user/HomeBanner.jsx
@@ -6,7 +6,7 @@ export default function HomeBanner() {
         <div className="carousel w-full h-80">
   <div id="slide1" className="carousel-item relative w-full">
     <img
-      src="https://www.rvrentacar.com/wp-content/uploads/2024/04/Untitled-design-10.png"
+      src="https://www.dollar.com/Images/1/9/8/%7B19828918-6976-4907-9343-2C6F210F6775%7D997EF5886468F3E677315D768E56C9282DEA66D469A6A937E31A33C2E3E5D829.jpg"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" className="btn btn-circle">❮</a>
@@ -15,7 +15,7 @@ export default function HomeBanner() {
   </div>
   <div id="slide2" className="carousel-item relative w-full">
     <img
-      src="https://www.autostrad.com/admin_module/uploads/offer/f4e2551dbe25c33c1a5613c1be2ce0ee.jpg"
+      src="https://stimg.cardekho.com/images/carexteriorimages/930x620/Mahindra/Thar-ROXX/8438/1723692413550/front-left-side-47.jpg"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" className="btn btn-circle">❮</a>
@@ -24,7 +24,7 @@ export default function HomeBanner() {
   </div>
   <div id="slide3" className="carousel-item relative w-full">
     <img
-      src="https://www.dollar.com/Images/1/9/8/%7B19828918-6976-4907-9343-2C6F210F6775%7D997EF5886468F3E677315D768E56C9282DEA66D469A6A937E31A33C2E3E5D829.jpg"
+      src="https://content.carlelo.com/uploads/blog_img/1693036032.webp"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" className="btn btn-circle">❮</a>
